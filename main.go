359d034a5344ae10d3875563cd95b25ee79bdfe4
Parham Alvani <parham.alvani@gmail.com>
2020-09-18 21:18:32 +0430

    feat: Improve Command Structure

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
 import (
-	"os"
-
-	"github.com/1995parham/zamaneh/app"
-	"github.com/sirupsen/logrus"
+	"github.com/1995parham/zamaneh/cmd"
 )
 
 func main() {
-	topic := "untitled"
-	if len(os.Args) > 1 {
-		topic = os.Args[1]
-	}
-
-	a, err := app.New(topic)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
-	if err := a.Run(); err != nil {
-		logrus.Fatal(err)
-	}
+	cmd.Execute()
 }
