2e87c1ff235509d1425cccd92a350250ee578e64
Parham Alvani <parham.alvani@gmail.com>
2021-07-24 08:54:14 +0430

    feat: Working Pomodo Timer

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,23 @@ enum State {
     Fun,
 }
 
+const WORKING_PERIOD_MINS: u64 = 25;
+const FUN_PERIOD_MINS: u64 = 5;
+
 #[tokio::main]
 async fn main() {
+    let mut args = std::env::args();
+    let topic = args.nth(1).unwrap_or_else(|| "-".to_string());
+
     let mut sec_one_interval = time::interval(time::Duration::from_secs(1));
-    let mut working_interval = time::interval(time::Duration::from_secs(60));
-    let mut fun_interval = time::interval(time::Duration::from_secs(60));
+    let mut working_interval = time::interval(time::Duration::from_secs(WORKING_PERIOD_MINS * 60));
+    let mut fun_interval = time::interval(time::Duration::from_secs(FUN_PERIOD_MINS * 60));
 
     let mut state = State::Fun;
 
     let mut secs = 0;
 
+    println!("you are working on {}", topic);
     loop {
         match state {
             State::Working => {
@@ -34,8 +41,8 @@ async fn main() {
                     _ = working_interval.tick() => {
                         secs = 0;
                         state = State::Fun;
-                        println!("time to fun");
-                        fun_interval = time::interval(time::Duration::from_secs(60));
+                        println!("time to have fun... {}min", FUN_PERIOD_MINS);
+                        fun_interval = time::interval(time::Duration::from_secs(FUN_PERIOD_MINS * 60));
                         fun_interval.tick().await;
                     },
                 }
@@ -57,8 +64,8 @@ async fn main() {
                     _ = fun_interval.tick() => {
                         secs = 0;
                         state = State::Working;
-                        println!("time to work");
-                        working_interval = time::interval(time::Duration::from_secs(60));
+                        println!("time to work... {}min", WORKING_PERIOD_MINS);
+                        working_interval = time::interval(time::Duration::from_secs(WORKING_PERIOD_MINS * 60));
                         working_interval.tick().await;
                     },
                 }
