43223879083b749ca73d81b1dc9423615ef677f9
Parham Alvani <1995parham@tuta.io>
2021-08-19 21:59:05 +0430

    feat: Handle Events with Tokio Channels which are awesome

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+use std::io::{stdin, stdout};
+use std::process::exit;
 use termion::cursor;
-use tokio::time;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use tokio::sync::mpsc::channel;
+use tokio::{select, time};
+
+enum Event {
+    Pause,
+    Quit,
+}
 
 #[tokio::main]
 async fn main() {
@@ -10,25 +21,74 @@ async fn main() {
 
     let mut secs = 0;
 
+    let (tx, mut rx) = channel::<Event>(1);
+
+    let stdin = stdin();
+    let stdout = stdout().into_raw_mode().unwrap();
+    stdout.suspend_raw_mode().unwrap();
+
+    let mut is_pause = false;
+
+    tokio::spawn(async move {
+        for c in stdin.keys() {
+            match c.unwrap() {
+                Key::Char('q') => {
+                    let _ = tx.send(Event::Quit).await;
+                }
+                Key::Char(' ') => {
+                    let _ = tx.send(Event::Pause).await;
+                }
+                _ => {}
+            }
+        }
+    });
+
     println!(
         r#"{}
 Spending time with you is so precious,
 I love every minute that we are together.
-        "#,
+"#,
         cursor::Hide
     );
     println!("you are working on {}", topic);
-    loop {
-        sec_one_interval.tick().await;
-        secs += 1;
+    stdout.activate_raw_mode().unwrap();
 
-        print!("{}", termion::cursor::Save);
-        println!(
-            "{:0>2}:{:0>2}:{:0>2}",
-            (secs / 60) / 60,
-            (secs / 60) % 60,
-            secs % 60
-        );
-        print!("{}", termion::cursor::Restore);
+    loop {
+        select! {
+            Some(ev) = rx.recv() => {
+                match ev {
+                    Event::Pause => {
+                        is_pause = !is_pause;
+                    },
+                    Event::Quit => {
+                        stdout.suspend_raw_mode().unwrap();
+                        println!(
+                        "{}you are working for {:0>2}:{:0>2}:{:0>2} on {}",
+                        cursor::Show,
+                        (secs / 60) / 60,
+                        (secs / 60) % 60,
+                        secs % 60,
+                        topic
+                        );
+                        exit(0);
+                    }
+                }
+            },
+            _ = sec_one_interval.tick() => {
+                if !is_pause {
+
+                    secs += 1;
+
+                    print!("{}", termion::cursor::Save);
+                    println!(
+                    "{:0>2}:{:0>2}:{:0>2}",
+                    (secs / 60) / 60,
+                    (secs / 60) % 60,
+                    secs % 60
+                    );
+                    print!("{}", termion::cursor::Restore);
+                }
+            }
+        }
     }
 }
